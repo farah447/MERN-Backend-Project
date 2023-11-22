4c0aa14a6f02d0dc4f92e7d82a15397712508428
Abdullah Alnaqeeb <akai_100@hotmail.com>
2023-11-22 12:29:25 +0300

    test

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,22 @@
 import express, { Application, Request, Response } from 'express'
-import { connectDB } from "./config/db";
-import { dev } from "./config";
+import { connectDB } from './config/db'
+import { dev } from './config'
 
-import { errorHandler } from "./middlewares/errorHandler";
-import ordersRouter from './routers/orderRoute';
-import categoriesRouter from './routers/categoryRoute';
+import { errorHandler } from './middlewares/errorHandler'
+import ordersRouter from './routers/orderRoute'
+import categoriesRouter from './routers/categoryRoute'
 import usersRouter from './routers/userRoute'
-import { createHttpError } from "./util/createHTTPError";
-import productRoute from "./routers/productRoute";
+import { createHttpError } from './util/createHTTPError'
+import productRoute from './routers/productRoute'
 
 const app: Application = express()
 
-const port: number = dev.app.port || 3003;
+const port: number = dev.app.port || 3003
 
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-  connectDB();
-});
+  console.log(`Server is running at http://localhost:${port}`)
+  connectDB()
+})
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -25,14 +25,14 @@ app.get('/', (req: Request, res: Response) => {
   res.json({ message: 'Health checkup' })
 })
 
-
-app.use("/products", productRoute);
+app.use('/products', productRoute)
 app.use('/orders', ordersRouter)
-app.use("/categories", categoriesRouter);
-app.use("/users", usersRouter);
-
+app.use('/categories', categoriesRouter)
+app.use('/users', usersRouter)
 
 app.use((req, res, next) => {
-  next(createHttpError(404, "Route Not Found"));
-});
-app.use(errorHandler);
+  next(createHttpError(404, 'Route Not Found'))
+})
+app.use(errorHandler)
+
+console.log('first')

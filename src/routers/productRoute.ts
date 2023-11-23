cce6ef886f37b0215af38a8b5735ee18eaad92da
Abdullah Alnaqeeb <akai_100@hotmail.com>
2023-11-23 14:03:18 +0300

    Search by title

--- src/routers/productRoute.ts
+++ src/routers/productRoute.ts
@@ -1,26 +1,29 @@
-import { Router } from "express";
+import { Router } from 'express'
 
 import {
-    createSingleProduct,
-    deleteProductBySlug,
-    getAllProducts,
-    getProductsBySlug,
-    updateProductBySlug,
-} from "../controllers/productController";
-import { upload } from "../middlewares/uploadFile";
+  createSingleProduct,
+  deleteProductBySlug,
+  getAllProducts,
+  getProductsBySlug,
+  searchProductsByTitle,
+  updateProductBySlug,
+} from '../controllers/productController'
+import { upload } from '../middlewares/uploadFile'
 
-const router = Router();
+const router = Router()
 
-router.get("/", getAllProducts);
+router.get('/', getAllProducts)
 
-router.get("/:slug", getProductsBySlug);
+router.get('/:slug', getProductsBySlug)
+
+router.get('/search/:title', searchProductsByTitle)
 
 //router.get("/:id", getSingleProduct);
 
-router.post("/", upload.single('image'), createSingleProduct);
+router.post('/', upload.single('image'), createSingleProduct)
 
-router.delete("/:slug", deleteProductBySlug);
+router.delete('/:slug', deleteProductBySlug)
 
-router.put("/:slug", updateProductBySlug);
+router.put('/:slug', updateProductBySlug)
 
-export default router;
+export default router

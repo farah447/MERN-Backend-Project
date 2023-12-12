0c96e7278fec0e629c4872f842914759e708be38
Farah <alharbi.farah@hotmail.com>
2023-12-12 21:38:40 +0300

    add vercel file and mongoDB link

--- api/server.ts
+++ api/server.ts
@@ -1,30 +1,31 @@
 import cookieParser from 'cookie-parser'
 import express, { Application, Request, Response } from 'express'
 
-import { dev } from './config'
-import { connectDB } from './config/db'
-import { errorHandler } from './middlewares/errorHandler'
-import authRoute from './routers/authRoute'
-import categoriesRouter from './routers/categoryRoute'
-import ordersRouter from './routers/orderRoute'
-import productRoute from './routers/productRoute'
-import usersRouter from './routers/userRoute'
-import { createHttpError } from './util/createHTTPError'
+import { dev } from '../src/config'
+import { connectDB } from '../src/config/db'
+import { errorHandler } from '../src/middlewares/errorHandler'
+import authRoute from '../src/routers/authRoute'
+import categoriesRouter from '../src/routers/categoryRoute'
+import ordersRouter from '../src/routers/orderRoute'
+import productRoute from '../src/routers/productRoute'
+import usersRouter from '../src/routers/userRoute'
+import { createHttpError } from '../src/util/createHTTPError'
 
 const app: Application = express()
 
 const port: number = dev.app.port || 3003
 
+connectDB()
+
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`)
-  connectDB()
 })
 app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req: Request, res: Response) => {
-  res.status(200).json({ message: 'Health checkup' })
+  res.json({ message: 'Health checkup' })
 })
 
 app.use('/products', productRoute)
--- src/config/index.ts
+++ src/config/index.ts
@@ -3,16 +3,16 @@ import 'dotenv/config'
 export const dev = {
   app: {
     port: Number(process.env.PORT) || 3003,
-    defaultUserImagePath: process.env.DEFAULT_USER_IMAGE_PATH || 'public/images/users/default.png',
+    defaultUserImagePath: process.env.DEFAULT_USER_IMAGE_PATH || '',
     defaultProductsImagePath:
-      process.env.DEFAULT_PRODUCTS_IMAGE_PATH || 'public/images/products/default.png',
-    jwtUserActivationKey: process.env.JWT_ACCOUNT_ACTIVATION_KEY || 'aaaa',
-    jwtAccessKey: process.env.JWT_ACCESS_KEY || 'farah',
-    stmpUsername: process.env.STMP_USERNAME || 'asmasaad3778@gmail.com',
-    stmpPassword: process.env.STMP_PASSWORD || 'zskx btdw msij pflj',
-    JWT_SECRET: process.env.JWT_SECRET || 'JWT_SECRET',
+      process.env.DEFAULT_PRODUCTS_IMAGE_PATH || '',
+    jwtUserActivationKey: process.env.JWT_ACCOUNT_ACTIVATION_KEY || '',
+    jwtAccessKey: process.env.JWT_ACCESS_KEY || '',
+    stmpUsername: process.env.STMP_USERNAME || '',
+    stmpPassword: process.env.STMP_PASSWORD || '',
+    JWT_SECRET: process.env.JWT_SECRET || '',
   },
   db: {
-    url: process.env.MONGODB_URL || 'mongodb://127.0.0.1:27017/e-commerce-backend-project',
+    url: process.env.MONGODB_URL || '',
   },
 }
